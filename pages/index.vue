9f3e2ad0f32902489aacb2a31f28e7be67f33274
ekypow <lucas.schrever@gmail.com>
2022-01-18 18:41:06 +0100

    Nuxt linked w/ Prismic

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,18 @@
 <template>
-  <Tutorial/>
+  <h1>Hellooow!</h1>
 </template>
 
 <script>
-export default {
-  name: 'IndexPage'
+export default { 
+  name: 'IndexPage',
+  async asyncData({ $prismic, params, error }) {
+    const document = await $prismic.api.query('')
+    console.log(document)
+    if (document) {
+      return { document }
+    } else {
+      error({ statusCode: 404, message: 'Page not found' })
+    }
+  }
 }
 </script>
