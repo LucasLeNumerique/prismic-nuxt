a6757407498a3ddb67f85e35c6baba087e9ead59
Ulrich NGAKUI NGAHA <57942205+ngakui@users.noreply.github.com>
2022-01-21 20:25:11 +0100

    Add HomePage, Article and Contact

--- pages/index.vue
+++ pages/index.vue
@@ -1,58 +1,91 @@
 <template>
-  <div class="page-container md-layout-row">
-    <md-app>
-      <md-app-toolbar class="md-primary">
-        <span class="md-title">My Title</span>
+  <div class="page-container">
+    <md-app md-waterfall md-mode="fixed-last">
+      <md-app-toolbar :style="'background-image: url('+home_data['background-image'].url+');'" class="md-large md-dense md-primary">
+        <div class="md-toolbar-row">
+          <div class="md-toolbar-section-start">
+
+            <span class="md-title">
+              <img :src="logo.url" :width="logo.dimensions.width" :height="logo.dimensions.height" alt="">
+            </span>
+          </div>
+          <div v-for="item in home_data.headline1" :key="item.id">
+            <h1 class="md-display-2" v-if="item.type === 'heading1'"> {{item.text}}</h1>
+          </div>
+        </div>
+
+        <div class="md-toolbar-row md-toolbar-section-end">
+          <md-tabs class="md-primary">
+            <md-tab v-on:click="thepage('home')" id="tab-home" md-label="Accueil"></md-tab>
+            <md-tab v-on:click="thepage('article')" id="tab-pages" md-label="Articles"></md-tab>
+            <md-tab v-on:click="thepage('contact')" id="tab-posts" md-label="Contact"></md-tab>
+            <!-- <md-tab id="tab-favorites" md-label="Favorites"></md-tab> -->
+          </md-tabs>
+        </div>
       </md-app-toolbar>
-
-      <md-app-drawer md-permanent="full">
-        <md-toolbar class="md-transparent" md-elevation="0">
-          Navigation
-        </md-toolbar>
-
-        <md-list>
-          <md-list-item>
-            <md-icon>move_to_inbox</md-icon>
-            <span class="md-list-item-text">Inbox</span>
-          </md-list-item>
-
-          <md-list-item>
-            <md-icon>send</md-icon>
-            <span class="md-list-item-text">Sent Mail</span>
-          </md-list-item>
-
-          <md-list-item>
-            <md-icon>delete</md-icon>
-            <span class="md-list-item-text">Trash</span>
-          </md-list-item>
-
-          <md-list-item>
-            <md-icon>error</md-icon>
-            <span class="md-list-item-text">Spam</span>
-          </md-list-item>
-        </md-list>
-      </md-app-drawer>
-
+        
       <md-app-content>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam, similique, quo recusandae placeat dicta asperiores modi sint ea repudiandae maxime? Quae non explicabo, neque.
+        <home-page :data="home_data" v-if="page === 'home'"/>
+        <article-page v-if="page === 'article'"/>
+        <contact-page v-if="page === 'contact'"/>
       </md-app-content>
     </md-app>
   </div>
 </template>
 
 <script>
+import HomePage from './HomePage.vue';
+import ArticlePage from './ArticlePage.vue';
+import ContactPage from './ContactPage.vue';
+
+
 
 export default { 
   name: 'IndexPage',
-  async asyncData({ $prismic, params, error }) {
-    const document = await $prismic.api.query('')
-    console.log(document)
-    if (document) {
-      return { document }
-    } else {
-      error({ statusCode: 404, message: 'Page not found' })
+  components: {
+    HomePage,
+    ContactPage,
+    ArticlePage
+  },
+  methods: {
+    thepage (newpage) {
+        this.page = newpage
+        console.log(this.page)
+      // return this.page;
     }
+  },
+  
+    async asyncData({ $prismic, params, error }) {
+      let home = await $prismic.api.getSingle('homepage')
+      let menu = await $prismic.api.getSingle('menu')
+      console.log(JSON.parse(JSON.stringify(home.data['background-image'])))
+      if (home) {
+        return {
+          page: 'home',
+          logo: menu.data.logo,
+          home_data: home.data
+        } 
+      } else {
+        error({ statusCode: 404, message: 'Page not found' })
+      }
   }
 }
 </script>
 
+
+<style lang="css" >
+  .md-app {
+    height: 100vh;
+    border: 1px solid rgba(#000, .12);
+  }
+
+  .md-display-2 {
+    text-align: center;
+    color: #E19F3B !important;
+  }
+   
+  .md-drawer {
+    width: 230px;
+    max-width: calc(100vw - 125px);
+  }
+</style>
